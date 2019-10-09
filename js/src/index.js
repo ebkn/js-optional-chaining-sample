2fb3da9b4b5cc0998344c35ce6c409bf856a7e3e
ebkn <ktennis.mqekr12@gmail.com>
2019-10-09 22:49:20 +0900

    fix: sample code

--- js/src/index.js
+++ js/src/index.js
@@ -1,6 +1,7 @@
 const x = { a: 'foo', b: 'bar' };
+
 console.log(x?.a);
 console.log(x?.b);
 
-console.log(x.c);
+console.log(x?.c?.d?.e);
 // console.log(x?.c ?? 'not found');
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -4,4 +4,4 @@ console.log(x?.a);
 console.log(x?.b);
 
 console.log(x.c);
-console.log(x?.c ?? 'not found');
+console.log(x?.c?.d?.e ?? 'not found');
