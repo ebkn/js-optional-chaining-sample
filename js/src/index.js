79dad25b25ac2dd60da34563a15c51b255bb14e8
ebkn <ktennis.mqekr12@gmail.com>
2019-10-09 22:31:31 +0900

    init commit

---
+++ js/src/index.js
@@ -0,0 +1,6 @@
+const x = { a: 'foo', b: 'bar' };
+console.log(x?.a);
+console.log(x?.b);
+
+console.log(x.c);
+// console.log(x?.c ?? 'not found');
---
+++ ts/src/index.ts
@@ -0,0 +1,7 @@
+const x: any = { a: 'foo', b: 'bar' };
+
+console.log(x?.a);
+console.log(x?.b);
+
+console.log(x.c);
+console.log(x?.c ?? 'not found');
